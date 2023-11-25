b3c5d54d7841bb26ab330c55a8e13e31d3de722c
John Solly <jsolly@protonmail.com>
2023-11-25 09:31:45 -0500

    wip

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, render_template
 
 app = Flask(__name__)
-CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 @app.route('/new-todo')
 def new_todo():
