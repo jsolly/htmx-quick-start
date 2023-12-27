2ed664375a7019e000596a90764fecbd469c741c
John Solly <jsolly@protonmail.com>
2023-12-27 13:26:30 -0500

    remove unused file

--- lambda_function.py
+++
@@ -1,6 +0,0 @@
-def lambda_handler(event, context):
-    return {
-        'headers': {'content-type': 'text/html'},
-        'statusCode': 200,
-        'body': "<li><input type='checkbox'>New todo from Amz Lambda!</li>"
-    }
