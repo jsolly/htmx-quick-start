0635c93eca20f83a32dd0f8965331b94bc6492fa
John Solly <jsolly@protonmail.com>
2023-12-27 13:25:18 -0500

    Add lambda function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,12 @@ def index():
 @app.route('/new-todo')
 def new_todo():
     return "<li><input type='checkbox'> new todo</li>"
+
+
+@app.route('/lambda-todo')
+def lambda_todo():
+    response = requests.get("")
+    return response.text
 
 if __name__ == '__main__':
     app.run(debug=True)
